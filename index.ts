dd077a2b4a99ec541248b162dd67dc28078e230a
leantrim <leutrimbunjaku5@gmail.com>
2022-12-07 18:32:39 +0100

    removed dotenv

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import dotenv from 'dotenv';
 import client from "./discord/bot"
 import discordbot from "./routes/discordbot";
 import mongoose from "mongoose";
@@ -15,7 +14,6 @@ mongoose
 
 app.listen(8000, () => console.log("Listening on port 8000..."));
 
-dotenv.config();
 
 // Initate Discord bot
 client.login(process.env.DISCORD_TOKEN);
