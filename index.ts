eea3e52040b9c3ff71f4ddb4b2aac23117e8d337
leantrim <leutrimbunjaku5@gmail.com>
2022-12-07 18:45:37 +0100

    use require to import

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import client from "./discord/bot"
-import discordbot from "./routes/discordbot";
-import mongoose from "mongoose";
-import express from "express";
+const client = require("./discord/bot");
+const discordbot = require("./discord/discordbot");
+const mongoose = require("mongoose");
+const express = require("express");
+
 const app = express();
 
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use("/api/discordbot", discordbot);
 mongoose
   .connect("mongodb://localhost/lean-bot")
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.log("Could not connect to MongoDB...", err));
+  .catch((err: any) => console.log("Could not connect to MongoDB...", err));
 
 app.listen(8000, () => console.log("Listening on port 8000..."));
 
