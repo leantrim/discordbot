f5f7984890de280730d0782691be6d9ca66df6d1
lean bun <leutrimbunjaku5@gmail.com>
2022-12-25 14:27:32 +0000

    removed the mongodb Error

--- index.ts
+++ index.ts
@@ -17,13 +17,13 @@ const MONGODB = {
   port: process.env.MONGODB_PORT,
   db: process.env.MONGODB_DB,
 };
+mongoose.set('strictQuery', false);
 mongoose
   .connect(
     `mongodb://${MONGODB.user}:${MONGODB.password}@${MONGODB.ip}:${MONGODB.port}/${MONGODB.db}?authSource=${MONGODB.db}&authMechanism=DEFAULT`,
   )
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.log('Could not connect to MongoDB...', err));
-mongoose.set('strictQuery', false);
 
 app.listen(8000, () => console.log('Listening on port 8000...'));
 
